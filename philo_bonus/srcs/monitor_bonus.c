1b34e20e48a11bd1de056f6a69c0d9e36ae047b4
Yamato Hokari <yamatohokari@gmail.com>
2023-01-15 17:58:05 +0900

    added

--- philo_bonus/srcs/monitor_bonus.c
+++ philo_bonus/srcs/monitor_bonus.c
@@ -6,22 +6,22 @@
 /*   By: yahokari <yahokari@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 17:19:58 by yahokari          #+#    #+#             */
-/*   Updated: 2023/01/15 16:46:34 by yahokari         ###   ########.fr       */
+/*   Updated: 2023/01/15 17:41:22 by yahokari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"philosophers_bonus.h"
 
-static void	check_death(t_philos *philo, t_vars *vars, int id)
+static void	check_death(t_vars *vars, int id)
 {
 	ssize_t	time_of_death;
 	ssize_t	timestamp;
 	ssize_t	i;
 
 	timestamp = get_timestamp();
-	sem_wait(philo->check);
-	time_of_death = philo->last_meal + vars->time_to_die;
-	sem_post(philo->check);
+	sem_wait(vars->philos[id - 1].check);
+	time_of_death = vars->philos[id - 1].last_meal + vars->time_to_die;
+	sem_post(vars->philos[id - 1].check);
 	if (timestamp >= time_of_death)
 	{
 		print_state(vars->print, DIED, timestamp - vars->initial_time, id);
@@ -34,30 +34,28 @@ static void	check_death(t_philos *philo, t_vars *vars, int id)
 	}
 }
 
-static void	check_eaten(t_philos *philo, t_vars *vars)
+static void	check_eaten(t_vars *vars, ssize_t id)
 {
 	static bool	is_sem_post_sent;
 
 	if (vars->option_set == false)
 		return ;
-	sem_wait(philo->check);
-	if (philo->num_ate >= vars->num_must_eat && is_sem_post_sent == false)
+	sem_wait(vars->philos[id - 1].check);
+	if (vars->philos[id - 1].num_ate >= vars->num_must_eat \
+		&& is_sem_post_sent == false)
 	{
 		sem_post(vars->check_end);
 		is_sem_post_sent = true;
 	}
-	sem_post(philo->check);
+	sem_post(vars->philos[id - 1].check);
 }
 
-static void	act_monitor(t_philos *philo, t_vars *vars)
+static void	act_monitor(t_vars *vars, ssize_t id)
 {
-	int	id;
-
-	id = philo->id;
 	while (true)
 	{
-		check_death(philo, vars, id);
-		check_eaten(philo, vars);
+		check_death(vars, id);
+		check_eaten(vars, id);
 		usleep(MILISECOND);
 	}
 }
@@ -66,9 +64,11 @@ void	philo_and_monitor(t_philos *philo)
 {
 	t_vars		*vars;
 	pthread_t	thread;
+	ssize_t		id;
 
 	vars = philo->vars;
+	id = philo->id;
 	pthread_create(&thread, NULL, &act_philo, philo);
-	act_monitor(philo, vars);
+	act_monitor(vars, id);
 	pthread_detach(thread);
 }
